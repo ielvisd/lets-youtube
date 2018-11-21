f2afa7e12ce6d10f841e8a11fae90775b23a350a
Elvis Ibarra <ielvisd@gmail.com>
2018-11-20 23:05:43 -0800

    Youtube Auth Login Works

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.json({ api: 'up' });
+});
+
+server.get('/test', function(req, res) {
+  res.sendFile('/quickstart.html', { root: __dirname });
+});
+
+server.listen(9000, () => console.log('\n== Port 9k ==\n'));
