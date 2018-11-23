60242fcc85c964a9e688c5b7b460099e12b98701
Elvis Ibarra <ielvisd@gmail.com>
2018-11-23 15:28:02 -0800

    Gets access token (check console.log) but stuck with getting it to talk to the API.

--- app/src/views/App.js
+++ app/src/views/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import '../App.css';
 
+import { GoogleLogin } from 'react-google-login';
+
 class App extends Component {
   render() {
+    const responseGoogle = response => {
+      console.log(response);
+    };
+
     return (
       <body>
         <h1>Let's Youtube</h1>
+
+        <GoogleLogin
+          clientId="997089399413-p7h0oiugn12uf21v1mnii4n6k8fphmfn.apps.googleusercontent.com"
+          buttonText="Login"
+          scope="https://www.googleapis.com/auth/youtube"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
       </body>
     );
   }
---
+++ app/src/views/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <body>
+        <h1>Are we logged in?</h1>
+      </body>
+    );
+  }
+}
+export default App;
